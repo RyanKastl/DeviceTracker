837c3005e558390c5e552c02f5c3438476e20a18
Ryan <ryankastl16@gmail.com>
2019-05-01 01:10:14 -0500

    Incorporated Packet Sniffing into client

--- client.py
+++ client.py
@@ -1,11 +1,22 @@
 import requests
+import pyshark
 
 point = "http://127.0.0.1:5000/tracker/"
-
 key = "uh suh dude"
 
-data = {'mssg': 'heyo'}
+capture = pyshark.LiveCapture(interface='eth0')
+capture.set_debug()
 
-r = requests.post(url = point, data = data)
+target = "7c:d9:5c:b4:dd:25"
 
-print(r.text)
+for packet in capture.sniff_continuously():
+	src = packet['ETH'].src
+	dst = packet['ETH'].dst
+	
+	if (src == target or dst == target):
+		data = {'mssg': 'Client 1 found Pixel 3 XL'}
+		r = requests.post(url = point, data = data)
+		print(r.text)
+
+
+
