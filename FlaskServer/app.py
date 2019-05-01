38aceebd53fe722e099f48819e888799cfb0082f
root <root@Rhino.localdomain>
2019-05-01 01:01:02 -0500

    Set up post endpoint, along with client post example

--- FlaskServer/app.py
+++ FlaskServer/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from flask import request
 app = Flask(__name__)
 
+key = "uh suh dude"
 
 @app.route('/')
 def home():
@@ -10,6 +12,13 @@ def home():
 @app.route('/about/')
 def about():
     return render_template('about.html')
+
+@app.route('/tracker/', methods=['POST'])
+def printData():
+	if request.method == 'POST':
+		data = request.form
+		print(data)
+	return 'sup', 200
 
 
 if __name__ == '__main__':
--- client.py
+++ client.py
@@ -1,31 +1,11 @@
-import socket
-import sys
+import requests
 
-clientID = 1
+point = "http://127.0.0.1:5000/tracker/"
 
-HOST, PORT = "localhost", 9999
-data = " ".join(sys.argv[1:])
-data = "Device found at client 1"
+key = "uh suh dude"
 
-# Create a socket (SOCK_STREAM means a TCP socket)
+data = {'mssg': 'heyo'}
 
+r = requests.post(url = point, data = data)
 
-
-mssg = ""
-
-while(mssg != "quit"):
-    mssg = input()
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        # Connect to server and send data
-        sock.connect((HOST, PORT))
-        sock.sendall(bytes(mssg + "\n", "utf-8"))
-
-        # Receive data from the server and shut down
-        # received = str(sock.recv(1024), "utf-8")
-    finally:
-        sock.close()
-
-# print("Sent:     {}".format(data))
-# print("Received: {}".format(received))
+print(r.text)
