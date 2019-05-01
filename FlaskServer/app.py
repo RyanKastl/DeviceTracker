05e306d1df93b9725150bb1c07f355a954e4bd27
root <root@Rhino.localdomain>
2019-05-01 00:25:09 -0500

    Added example of passing python data to html

--- FlaskServer/app.py
+++ FlaskServer/app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    testVar = "henlo"
+    return render_template('home.html', testVar = testVar)
 
 @app.route('/about/')
 def about():
-	return render_template('about.html')
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
